1064ba97102a8cc1ad1aee59500a95cc6965e57e
Ihar Levachkov <pokerlis@gmail.com>
2024-03-08 17:57:52 +0300

    feat: add solution to mergeObjects

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -33,8 +33,19 @@ function shallowCopy(obj) {
  *    mergeObjects([{a: 1, b: 2}, {b: 3, c: 5}]) => {a: 1, b: 5, c: 5}
  *    mergeObjects([]) => {}
  */
-function mergeObjects(/* objects */) {
-  throw new Error('Not implemented');
+function mergeObjects(objects) {
+  let mergObj = {};
+  objects.forEach((obj) => {
+    const arrEntries = Object.entries(obj);
+    for (let i = 0; i < arrEntries.length; i += 1) {
+      const [key, value] = arrEntries[i];
+      if (Object.hasOwn(mergObj, key)) {
+        mergObj[key] += value;
+      }
+    }
+    mergObj = Object.assign(obj, mergObj);
+  });
+  return mergObj;
 }
 
 /**
